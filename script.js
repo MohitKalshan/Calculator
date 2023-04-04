fd634544c10edf45bf3c4bf4f2f24c9b41c0bdae
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-04-04 11:40:15 +0530

    Code Update

--- script.js
+++ script.js
@@ -3,109 +3,37 @@ var buttons = document.getElementsByClassName("buttons");
 var operand1 = 0;
 var operand2 = null;
 var operator = null;
-var string = '';
-Array.from(buttons).forEach((button)=>{
+var screenvalue = "";
 
-  button.addEventListener("click",(e)=>{
-   
-    if(e.target.innerHTML=="="){
-      string = eval(string);
-      document.getElementById("display").innerHTML = string;
-
-    }else if(e.target.innerHTML== "AC"){
-      string = "";
-      document.getElementById("display").innerHTML = string;
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    var value = this.getAttribute("data-value");
+    if (value == "AC") {
+      // operand1 = parseFloat(display.textContent);//
+      var screenvalue = " ";
+      display.innerText = screenvalue;
+    } else if (value == "=") {
+      // operand2 = parseFloat(display.textContent);
+      // var ans = "operand1 + operand2";
+      display.innerText = eval(display.textContent);
+    } else {
+      display.innerText += value;
     }
-   else{
-    console.log(e.target);
-    string = string + e.target.innerHTML;
-    document.getElementById("display").innerHTML = string;
-   }
   });
+}
 
-});
-
-
-// Input through Calculator buttons
-
-// for (var i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener("click", function () {
-//     var value = this.getAttribute("data-value");
-
-//     if (value == "reset") {
-//       display.innerText = null;
-//     } else if (
-//       value == "+" ||
-//       value == "-" ||
-//       value == "*" ||
-//       value == "/" ||
-//       value == "%"
-//     ) {
-//       operator = value;
-//       operand1 = parseFloat(display.innerText);
-//       console.log("opera 1 = ", operand1);
-//        display.innerText = null;
-//     } else if (value == "=") {
-//       if (operator != null) {
-//         operand2 = parseFloat(display.innerText);
-//         display.innerText = eval(operand1 + " " + operator + " " + operand2);
-//       } else {
-//         display.innerText = "Error";
-//       }
-//     } else if (value == "sign") {
-//       display.innerText = eval(parseFloat(display.innerText) * -1);
-//     } else if (value == ".") {
-//       if (display.innerText.length && !display.innerText.includes(".")) {
-//         display.innerText += value;
-//       }
+// Array.from(buttons).forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     if (e.target.innerHTML == "=") {
+//       screenvalue = eval(string);
+//       document.getElementById("display").innerHTML = screenvalue;
+//     } else if (e.target.innerHTML == "AC") {
+//       screenvalue = "";
+//       document.getElementById("display").innerHTML = screenvalue;
 //     } else {
-//       display.innerText += value;
+//       console.log(e.target);
+//       screenvalue = screenvalue + e.target.innerHTML;
+//       document.getElementById("display").innerHTML = screenvalue;
 //     }
 //   });
-// }
-
-// // Input with Keys
-
-// document.addEventListener("keypress", function (event) {
-//   var key;
-//   key = event.keyCode;
-//   value = String.fromCharCode(key);
-//   console.log(key + " " + value);
-//   var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-//   if (
-//     value == "+" ||
-//     value == "-" ||
-//     value == "*" ||
-//     value == "/" ||
-//     value == "%"
-//   ) {
-//     operator = value;
-//     operand1 = parseFloat(display.innerText);
-//     display.innerText = null;
-//   } else if (key == "13") {
-//     if (operator != null) {
-//       operand2 = parseFloat(display.innerText);
-//       display.innerText = eval(operand1 + " " + operator + " " + operand2);
-//     } else {
-//       display.innerText = "Error";
-//     }
-//   } else if (value == ".") {
-//     if (display.innerText.length && !display.innerText.includes(".")) {
-//       display.innerText += value;
-//     }
-//   } else if (value in numArray) {
-//     display.innerText += value;
-//   }
-// });
-
-// // For Clearing display with delete Key
-
-// document.addEventListener("keydown", function (event) {
-//   var key;
-//   key = event.keyCode;
-
-//   if (key == "8") {
-//     display.innerText = null;
-//   }
 // });
